a1e56022f111812ed74238a04dea15ca8f3cc438
deksa99 <deja99@live.com>
2022-06-06 06:18:00 +0200

    consecutive misses event 2

--- tta-app/src/main/java/com/tta/app/model/events/TooMuchMissesEvent.java
+++ tta-app/src/main/java/com/tta/app/model/events/TooMuchMissesEvent.java
@@ -1,6 +1,7 @@
 package com.tta.app.model.events;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 import org.kie.api.definition.type.Role;
 import org.kie.api.definition.type.Role.Type;
@@ -11,9 +12,9 @@ public class TooMuchMissesEvent implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String message;
-	private Long trainingId;
+	private UUID trainingId;
 	
-	public TooMuchMissesEvent(Long trainingId) {
+	public TooMuchMissesEvent(UUID trainingId) {
 		super();
 		this.trainingId = trainingId;
 	}
@@ -24,10 +25,10 @@ public class TooMuchMissesEvent implements Serializable {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	public Long getTrainingId() {
+	public UUID getTrainingId() {
 		return trainingId;
 	}
-	public void setTrainingId(Long trainingId) {
+	public void setTrainingId(UUID trainingId) {
 		this.trainingId = trainingId;
 	}
 }
