7d21fa8577472723125d8717c2d46972a4d1c2e1
nedeljkovignjevic <vignjevic.nedeljko1999@gmail.com>
2022-06-26 18:10:01 +0200

    feat: Auth.

---
+++ tta-frontend/src/service/AuthenticationService.js
@@ -0,0 +1,33 @@
+   
+import axios from "axios";
+
+const API_URL = "http://localhost:8081/api/auth"
+
+class AuthenticationService {
+
+    getAuthorizedUser() {
+        return axios({
+            method: 'GET',
+            url: `${API_URL}/authority`
+        });
+    }
+
+    register(data) {
+        return axios({
+            method: 'POST',
+            url: `${API_URL}/register`,
+            data: data,
+        });
+    }
+
+    login(data) {
+        return axios({
+            method: 'POST',
+            url: `${API_URL}/login`,
+            data: data
+        });
+    }
+
+}
+
+export default new AuthenticationService();
