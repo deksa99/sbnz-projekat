c354222f67733fb1831a36b7761e99411a7e552c
nedeljkovignjevic <vignjevic.nedeljko1999@gmail.com>
2022-06-26 18:07:24 +0200

    feat: Auth.

--- tta-frontend/src/App.vue
+++ tta-frontend/src/App.vue
@@ -2,6 +2,29 @@
     <router-view/>
 </template>
 
-<style>
+<script>
+import axios from "axios"
 
-</style>
+export default {
+    beforeCreate() {
+        if (!localStorage.getItem('token'))
+            return;
+        
+        let token = localStorage.getItem('token');
+        axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+        console.log("Token: " +  axios.defaults.headers.common['Authorization']);
+        axios.interceptors.response.use(function (response) {
+            return response;
+        }, function (error) {
+            if (error.response.status === 401) {
+                console.log("usao");
+                this.$router.push({name: "Login"});
+            }
+            return Promise.reject(error);
+        });
+    },
+    created() {
+        this.$store.dispatch('loadAuthorizedUser');
+    },
+}
+</script>
--- tta-frontend/src/store/index.js
+++ tta-frontend/src/store/index.js
@@ -1,11 +1,41 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import AuthenticationService from '@/service/AuthenticationService.js';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+        user: {
+            firstName: undefined,
+            lastName: undefined,
+            email: undefined,
+            role: undefined,
+        }
+    },
+    mutations: {
+        SET_AUTHORIZED_USER (state, user) {
+            state.user.firstName = user.firstName;
+            state.user.lastName = user.lastName;
+            state.user.email = user.email;
+            state.user.role = user.role;
+        },
+    },
+    actions: {
+        loadAuthorizedUser( {commit} ) {
+            if (!localStorage.getItem('token'))
+                return;
+
+            let token = localStorage.getItem('token');
+
+            AuthenticationService.getAuthorizedUser(token)
+                .then(response => { 
+                    let user = JSON.parse(response.data);
+                    commit('SET_AUTHORIZED_USER', user); 
+                })
+                .catch(error => { console.log(error); });
+        }
+    },
+    modules: {},
 });
