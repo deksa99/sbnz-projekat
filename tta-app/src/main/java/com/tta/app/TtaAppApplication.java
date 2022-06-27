1bdf3cdf31763eb69e40d2738deaf437f0f36d95
deksa99 <deja99@live.com>
2022-06-27 04:17:01 +0200

    racket comparator

--- tta-app/src/main/java/com/tta/app/TtaAppApplication.java
+++ tta-app/src/main/java/com/tta/app/TtaAppApplication.java
@@ -7,6 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TtaAppApplication {
@@ -29,4 +31,14 @@ public class TtaAppApplication {
 		kScanner.start(10_000);
 		return kContainer;
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
 }
--- tta-app/src/main/java/com/tta/app/service/RacketService.java
+++ tta-app/src/main/java/com/tta/app/service/RacketService.java
@@ -1,10 +1,10 @@
 package com.tta.app.service;
 
+import com.tta.app.model.racket.Racket;
 import com.tta.app.model.racket.RacketForm;
-import com.tta.app.model.racket.RacketParams;
 
 public interface RacketService {
 
-	public RacketParams getRecommendation(RacketForm form);
+	public Racket getRecommendation(RacketForm form);
 
 }
